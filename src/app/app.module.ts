8b329e22ef33c74509430938adf31056e80b369f
ayeshaabbas12 <ayesha@aptechgdn.net>
2024-11-29 09:51:25 +0500

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
